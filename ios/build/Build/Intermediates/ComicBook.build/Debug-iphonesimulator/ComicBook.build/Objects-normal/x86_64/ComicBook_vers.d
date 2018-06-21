a32b25d210347c5f97e49529b367528c8d6d6564
RUNNING37 <2640091934@qq.com>
2018-06-21 21:46:55 +0800

    rm

--- ios/build/Build/Intermediates/ComicBook.build/Debug-iphonesimulator/ComicBook.build/Objects-normal/x86_64/ComicBook_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/liyuechun/Desktop/ComicBook/ios/build/Build/Intermediates/ComicBook.build/Debug-iphonesimulator/ComicBook.build/DerivedSources/ComicBook_vers.c
--- ios/build/Build/Intermediates/ComicBook.build/Debug-iphonesimulator/ComicBook.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/liyuechun/Desktop/ComicBook/ios/ComicBook/main.m \
-  /Users/liyuechun/Desktop/ComicBook/ios/ComicBook/AppDelegate.h
